71d76c0e6d19f95ed0c5b48b3d607ba659b706c9
vraj129 <vrajpatelv@protonmail.com>
2023-02-11 13:43:03 +0530

    Setting the route and controller for auth module

---
+++ src/auth/entrypoint/AuthRouter.ts
@@ -0,0 +1,31 @@
+import * as express from 'express';
+import IAuthRepository from '../domain/IAuthRepository';
+import IPasswordService from '../services/IPasswordService';
+import ITokenService from '../services/ITokenService';
+import SignInUseCase from '../usecases/SignInUseCase';
+import AuthController from './AuthController';
+
+
+export default class AuthRouter {
+    public static configure(authRepository: IAuthRepository,
+        tokenService: ITokenService,
+        passwordService: IPasswordService,
+    ): express.Router {
+        const router = express.Router();
+        let controller = AuthRouter.composeController(
+            authRepository, tokenService, passwordService
+        );
+        router.post('/signin', (req, res) => controller.signin(req, res));
+        return router;
+    }
+
+    private static composeController(
+        authRepository: IAuthRepository,
+        tokenService: ITokenService,
+        passwordService: IPasswordService,
+    ): AuthController {
+        const signInUseCase = new SignInUseCase(authRepository, passwordService);
+        const controller = new AuthController(signInUseCase, tokenService);
+        return controller;
+    }
+}
